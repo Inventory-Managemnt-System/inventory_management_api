a766dd578cadfe4b1c1fd5e054b63ac469213ccf
zeemeks <anthonymekwunye95@gmail.com>
2024-08-15 09:15:54 +0100

    Update ItemController.php
    modified

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -18,7 +18,7 @@ class ItemController extends Controller
     //
     public function index(): JsonResponse
     {
-        $items = NewItem::paginate(50);
+        $items = NewItem::paginate(60000);
         $allItems = NewItem::all();
 
         return response()->json([
