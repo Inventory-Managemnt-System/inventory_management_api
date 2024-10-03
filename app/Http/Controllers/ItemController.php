926b738a651aacfcd6d75500c50c7259a5cb9273
Uniek Ideas Consulting <165657807+Uniekideas@users.noreply.github.com>
2024-10-03 11:45:38 +0100

    more updates

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -240,9 +240,9 @@ class ItemController extends Controller
 
                 if($request->get('format') == 'excel'){
                     
-                    // $xlsxname = Carbon::now()->format('Ymdhms').'inventoryReport.xlsx';
-                    // $store = Excel::store(new ItemsExport($items), $xlsxname, 'public');
-                    // dd($store);
+                    $xlsxname = Carbon::now()->format('Ymdhms').'inventoryReport.xlsx';
+                    $store = Excel::store(new ItemsExport($items), $xlsxname, 'public');
+                    dd($store);
                     // if($store){
                     //     return response()->download(public_path('storage/'.$xlsxname));
                     // }
