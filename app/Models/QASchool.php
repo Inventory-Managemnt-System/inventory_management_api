cb845c9defdf08cbe23cd9897e331effc05c96bf
Obinna Ukpabi <askphntm@gmail.com>
2024-10-10 12:26:30 +0100

    Update QASchool.php

--- app/Models/QASchool.php
+++ app/Models/QASchool.php
@@ -2,11 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class QASchool extends Model
 {
     use HasFactory, SoftDeletes;
+
+
+
+    /**
+     * Get all of the schools for the QASchool
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function schools(): HasMany
+    {
+        return $this->hasMany(School::class);
+    }
+
+
+    /**
+     * Get the user that owns the QASchool
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
