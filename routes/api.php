628bfd4efdd4503228888948f674f716f278613d
Obinna Ukpabi <askphntm@gmail.com>
2024-11-11 14:45:59 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,10 @@ use App\Http\Controllers\UserSettingsController;
 |
 */
 
+Route::get("/", function(){
+    return ['welcome, we good'];
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
